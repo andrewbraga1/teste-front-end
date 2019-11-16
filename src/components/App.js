7ea5511b3d35a3d5ac1f87c1296c8f8515d246e9
drew <you@example.com>
2019-11-16 00:04:43 -0300

    implementado busca apos busca mesma consulta e com consultas diferentes

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,83 @@
 import React, { Component } from "react";
 import "../styles/App.css";
-import "./SearchBar"
+import "./SearchBar";
 import SearchBar from "./SearchBar";
-import youtube from '../services/youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
-import BottomScrollListener from 'react-bottom-scroll-listener';
- 
+import youtube from "../services/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+import BottomScrollListener from "react-bottom-scroll-listener";
 
 class App extends Component {
   state = {
-            searchFieldFromSearchBar:null,
-            nextPageToken: null,
-            videos: [],
-            selectedVideo: null
-        }
-        handleSubmit = async (searchFieldFromSearchBar) => {
-          if (this.state.searchFieldFromSearchBar == null) {
-            this.setState({
-              searchFieldFromSearchBar:searchFieldFromSearchBar
-            })
-          }
-          const response = await youtube.get('/search', {
-            params: this.state.nextPageToken == null ? {
-              ...youtube.defaults.params,
-              q: searchFieldFromSearchBar,
-            }: {
-              ...youtube.defaults.params,
-              pageToken : this.state.nextPageToken,
-              q: this.state.searchFieldFromSearchBar,
-            }
-            
-          })
-          //debugger
-          console.log(response);
-                      
-            this.setState({
-                videos: response.data.items,
-                nextPageToken:response.data.nextPageToken
-            })
-        };
-        handleVideoSelect = (video) => {
-            this.setState({selectedVideo: video})
-        }
+    searchFieldFromSearchBar: null,
+    nextPageToken: null,
+    videos: [],
+    selectedVideo: null
+  };
+  handleSubmit = async searchFieldFromSearchBar => {
+    if (
+      this.state.searchFieldFromSearchBar === null ||
+      (this.state.searchFieldFromSearchBar !== searchFieldFromSearchBar &&
+        searchFieldFromSearchBar !== undefined)
+    ) {
+      var changed =
+        this.state.searchFieldFromSearchBar !== searchFieldFromSearchBar
+          ? true
+          : false;
+      this.setState({
+        searchFieldFromSearchBar: searchFieldFromSearchBar,
+        nextPageToken: null,
+        videos: []
+      });
+    }
+    const element = document.querySelector("form");
+    element.classList.add("animated", "slideOutUp");
 
+    const response = await youtube.get("/search", {
+      params:
+        this.state.nextPageToken == null || changed
+          ? {
+              ...youtube.defaults.params,
+              q: searchFieldFromSearchBar
+            }
+          : {
+              ...youtube.defaults.params,
+              pageToken: this.state.nextPageToken,
+              q: this.state.searchFieldFromSearchBar
+            }
+    });
+
+    this.setState({
+      videos: this.state.videos.concat(response.data.items),
+      nextPageToken: response.data.nextPageToken
+    });
+  };
+  handleVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
 
   render() {
     return (
       <div className="App">
-       <SearchBar handleFormSubmit={this.handleSubmit}/>
+        <SearchBar handleFormSubmit={this.handleSubmit} />
         <div id="results">
-        <div className='ui grid'>
-          <div className="ui row">
-            {this.state.selectedVideo ? 
-              <div className="eleven wide column">
-                <VideoDetail video={this.state.selectedVideo} />
-              </div> : ''}
-              
+          <div className="ui grid">
+            <div className="ui row">
+              {this.state.selectedVideo ? (
+                <div className="eleven wide column">
+                  <VideoDetail video={this.state.selectedVideo} />
+                </div>
+              ) : (
+                ""
+              )}
+
               <div className="five wide column">
-                  
-                  <VideoList handleVideoSelect={this.handleVideoSelect} videos={this.state.videos}/>
+                <VideoList
+                  handleVideoSelect={this.handleVideoSelect}
+                  videos={this.state.videos}
+                />
               </div>
-              
-              </div>
+            </div>
           </div>
         </div>
         <BottomScrollListener onBottom={this.handleSubmit} />
@@ -72,5 +87,3 @@ class App extends Component {
 }
 
 export default App;
-
-
--- src/services/youtube.js
+++ src/services/youtube.js
@@ -1,14 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-var KEY = 'AIzaSyCFevdADumbPEsj-UEDESesryIuXLwR-6s';
+var KEY = "AIzaSyCFevdADumbPEsj-UEDESesryIuXLwR-6s";
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3/',
-    params: {
-        part: 'id,snippet',
-        //maxResults: 5, //para testes
-        key: KEY
-    }
-})
-
-
+  baseURL: "https://www.googleapis.com/youtube/v3/",
+  params: {
+    part: "id,snippet",
+    //maxResults: 1, //para testes
+    key: KEY
+  }
+});
